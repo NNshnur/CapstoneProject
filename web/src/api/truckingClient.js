e79139cac008724fc1f6b40158beb04eb97a9a05
Natalia Shnur <nataliashnur@gmail.com>
2023-06-07 16:12:14 -0500

    Update and Delete buttons work, working fixing funcitionality

--- web/src/api/truckingClient.js
+++ web/src/api/truckingClient.js
@@ -195,6 +195,22 @@ export default class TruckingClient extends BindingClass {
                     this.handleError(error, errorCallback)
                 }
             }
+    async deleteExpense(expenseId, errorCallback) {
+            try {
+                const token = await this.getTokenOrThrow("Only authenticated users can remove a profile.");
+                const response = await this.axiosClient.put(`expenses/removeExpense`, {
+                    expenseId: expenseId
+                }, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+                return response.data;
+            } catch (error) {
+                this.handleError(error, errorCallback)
+            }
+        }
 
     /**
      * Helper method to log the error and run any error functions.
--- web/src/pages/updateExpense.js
+++ web/src/pages/updateExpense.js
@@ -4,7 +4,7 @@ import Header from '../components/truckingHeader';
 import DataStore from "../util/DataStore";
 import 'bootstrap';
 
-class CreateExpense extends BindingClass {
+class UpdateExpense extends BindingClass {
     constructor() {
         super();
         this.bindClassMethods([
@@ -15,7 +15,7 @@ class CreateExpense extends BindingClass {
         'submit',
         'redirectEditProfile',
         'redirectAllExpenses',
-        'redirectCreateExpense',
+        'redirectUpdateExpense',
         'logout'], this);
         this.dataStore = new DataStore();
         this.header = new Header(this.dataStore);
@@ -33,21 +33,10 @@ class CreateExpense extends BindingClass {
 
      }
 
-//    mount() {
-//        document.getElementById('createExpense').addEventListener('click', this.submit);
-//        document.getElementById('createExpense').addEventListener('click', this.redirectCreateExpense);
-//        document.getElementById('logout').addEventListener('click', this.logout);
-//        document.getElementById('createExpense').addEventListener('click', this.submit);
-//        this.header.addHeaderToPage();
-//        this.client = new truckingClient();
-//        this.clientLoaded();
-//    }
-
-
 mount() {
-  const createExpenseButton = document.getElementById('createExpense');
-  if (createExpenseButton) {
-    createExpenseButton.addEventListener('click', this.redirectCreateExpense);
+  const updateExpenseButton = document.getElementById('updateExpense');
+  if (updateExpenseButton) {
+    updateExpenseButton.addEventListener('click', this.redirectUpdateExpense);
   }
 
   const logoutButton = document.getElementById('logout');
@@ -78,11 +67,11 @@ mount() {
 
 async submit(evt) {
   evt.preventDefault();
-  const createButton = document.getElementById('submitted');
+  const updateButton = document.getElementById('submitted');
   const errorMessageDisplay = document.getElementById('errorMessageDisplay');
   const truckId = document.getElementById('t_idz').value;
   const vendorName = document.getElementById('vName').value;
-  const category = document.getElementById('categoryCc').value; // Get the selected category value
+  const category = document.getElementById('categoryCc').value;
   const date = document.getElementById('dDate').value;
   const amount = document.getElementById('amo').value;
   const paymentType = document.getElementById('ptypeC').value;
@@ -128,11 +117,79 @@ async submit(evt) {
     const confirmModal = new bootstrap.Modal(document.getElementById('confirmModal'));
     confirmModal.show();
   } catch (error) {
-    createButton.innerText = 'Submit';
+    updateButton.innerText = 'Submit';
     errorMessageDisplay.innerText = `Error: ${error.message}`;
     errorMessageDisplay.classList.remove('hidden');
   }
 }
+
+async function editExpense(expenseId) {
+  try {
+    const expense = await this.client.getExpense(expenseId);
+    if (!expense) {
+      throw new Error('Expense not found.');
+    }
+    // Populate the form fields with the expense data
+    document.getElementById('t_idz').value = expense.truckId;
+    document.getElementById('vName').value = expense.vendorName;
+    document.getElementById('categoryCc').value = expense.category;
+    document.getElementById('dDate').value = expense.date;
+    document.getElementById('amo').value = expense.amount;
+    document.getElementById('ptypeC').value = expense.paymentType;
+
+    const editButton = document.getElementById('editConfirm');
+    editButton.addEventListener('click', () => saveChanges(expenseId));
+
+    // Redirect to updateExpense.html
+    const updateButton = document.getElementById('updateExpense');
+    updateButton.addEventListener('click', this.redirectUpdateExpense);
+
+    const editModal = new bootstrap.Modal(document.getElementById('editModal'));
+    editModal.show();
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+
+
+        async function saveChanges(expenseId) {
+            const truckId = document.getElementById('t_idz').value;
+            const vendorName = document.getElementById('vName').value;
+            const category = document.getElementById('categoryCc').value;
+            const date = document.getElementById('dDate').value;
+            const amount = document.getElementById('amo').value;
+            const paymentType = document.getElementById('ptypeC').value;
+
+            try {
+                // Update the expense with the new data
+                const updatedExpense = await this.client.updateExpense(
+                    expenseId,
+                    truckId,
+                    vendorName,
+                    category,
+                    date,
+                    amount,
+                    paymentType
+                );
+
+                console.log('Expense updated:', updatedExpense);
+
+                // Close the edit modal
+                const editModal = bootstrap.Modal.getInstance(document.getElementById('editModal'));
+                editModal.hide();
+
+                // Refresh the expenses list or perform any other necessary actions
+                // ...
+
+            } catch (error) {
+                console.error('Error:', error.message);
+            }
+        }
+
+  redirectUpdateExpense() {
+  window.location.href = '/updateExpense.html';
+   }
 
     redirectToExpenses() {
         console.log("redirectToExpenses");
@@ -140,7 +197,7 @@ async submit(evt) {
     }
     confirmRedirect() {
     window.location.href = '/expenses.html';
-    console.log("createExpense button clicked");
+    console.log("updateExpense button clicked");
     }
 
     redirectProfile(){
